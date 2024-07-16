16f74ec5c4449b4d91f31ea428ae1f02a7ca2718
Mohamed waleed <Mohamed waleed@DESKTOP-0AUSUD8>
2024-07-16 13:21:04 +0300

    Add project files.

---
+++ A. Summation/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+int n = int.Parse(Console.ReadLine());
+int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+int sum = 0;
+foreach (int i in arr)
+{
+    sum += i;
+}
+if (sum < 0)
+{
+    Console.WriteLine((sum) * -1);
+}
+else
+{
+    Console.WriteLine(sum);
+}
+Console.ReadLine();
